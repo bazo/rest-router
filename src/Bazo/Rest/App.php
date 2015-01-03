137d38ca04a0714b35477e87a016ff00039cd341
bazo <martin@bazo.sk>
2015-01-03 12:44:55 +0100

    leave the error handling to calling app. BC break

--- src/Bazo/Rest/App.php
+++ src/Bazo/Rest/App.php
@@ -2,13 +2,12 @@
 
 namespace Bazo\Rest;
 
+
 use Bazo\Rest\Middleware\MiddlewareInterface;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
-
 
 /**
  * @author Martin Bažík <martin@bazik.sk>
@@ -34,12 +33,11 @@ class App
 	/** @var array */
 	public $onException = [];
 
-
 	function __construct(LoggerInterface $logger = NULL, Request $req = NULL, Response $res = NULL)
 	{
-		$this->logger = $logger;
-		$this->req = !is_null($req) ? $req : Request::createFromGlobals();
-		$this->res = !is_null($res) ? $res : new Response;
+		$this->logger	 = $logger;
+		$this->req		 = !is_null($req) ? $req : Request::createFromGlobals();
+		$this->res		 = !is_null($res) ? $res : new Response;
 	}
 
 
@@ -67,23 +65,11 @@ class App
 			}
 		} catch (\Exception $e) {
 			$this->log($e->getMessage(), [], LogLevel::ERROR);
-			$this->sendErrorResponse($e);
 			foreach ($this->onException as $exceptionCallback) {
 				call_user_func($exceptionCallback, $e);
 			}
 		}
 		$this->log('Processed uri: ' . $this->req->getUri());
-	}
-
-
-	protected function sendErrorResponse(\Exception $e)
-	{
-		$this->res->setStatusCode(500);
-		$content = [
-			'error' => $e->getMessage()
-		];
-		$this->res->setContent(json_encode($content, JSON_PRETTY_PRINT));
-		$this->res->send();
 	}
 
 
